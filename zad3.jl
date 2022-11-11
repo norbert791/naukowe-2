411abeed44ea38473857f4c2088fb12ad814f754
Norbert Jaśniewicz <norbert.jasniewicz@protonmail.com>
2022-11-11 17:17:26 +0100

    Dodano rozwiązanie zadania 5

--- zad3.jl
+++ zad3.jl
@@ -11,10 +11,10 @@ function hilb(n::Int)
   # Usage: hilb(10)
   #
   # Pawel Zielinski
-          if n < 1
-           error("size n should be >= 1")
-          end
-          return [1 / (i + j - 1) for i in 1:n, j in 1:n]
+    if n < 1
+      error("size n should be >= 1")
+    end
+    return [1 / (i + j - 1) for i in 1:n, j in 1:n]
   end
 
   using LinearAlgebra
@@ -30,14 +30,16 @@ function matcond(n::Int, c::Float64)
 # Usage: matcond(10, 100.0)
 #
 # Pawel Zielinski
-        if n < 2
-         error("size n should be > 1")
-        end
-        if c< 1.0
-         error("condition number  c of a matrix  should be >= 1.0")
-        end
-        (U,S,V)=svd(rand(n,n))
-        return U*diagm(0 =>[LinRange(1.0,c,n);])*V'
+  if n < 2
+    error("size n should be > 1")
+  end
+  if c< 1.0
+    error("condition number  c of a matrix  should be >= 1.0")
+  end
+  (U,S,V)=svd(rand(n,n))
+  return U*diagm(0 =>[LinRange(1.0,c,n);])*V'
 end
 
-    
+function main()
+
+end
---
+++ zad5.jl
@@ -0,0 +1,50 @@
+#author: Norbert Jaśniewicz
+
+
+
+function generate_population_sequence(r :: T, p_0 :: T) where T <: Union{Float32, Float64}
+  function p(n :: Unsigned)
+    result :: T = p_0
+    while n > 0
+      result = result + r * result * (1.0 - result)
+      n -= 1
+    end
+    return result
+  end
+
+  return p
+end
+
+function generate_truncated_population_sequence(r :: T, p_0 :: T) where T <: Union{Float32, Float64}
+  function p(n :: Unsigned)
+    result :: T = p_0
+    for i in 1:n
+      result = result + r * result * (1.0 - result)
+      if i == 10
+        result = floor(result, digits=3)
+      end
+    end
+    return result
+  end
+
+  return p
+end
+
+function main()
+  r :: Float32 = 3.0
+  p_0 :: Float32 = 0.01
+  r64 :: Float64 = 3.0
+  p_064 :: Float64 = 0.01
+  number_of_iterations :: Unsigned = 40
+
+  fun32 = generate_population_sequence(r, p_0)
+  fun64 = generate_population_sequence(r64, p_064)
+  fun_trun32 = generate_truncated_population_sequence(r, p_0)
+
+  n :: Unsigned = 5
+  println("Float32,Float32_truncated,Float64")
+  println("$(fun32(number_of_iterations)),$(fun_trun32(number_of_iterations)),$(fun64(number_of_iterations))")
+
+end
+
+main()
